f80021b34ea6c195a93947401b50e0d1245e9f05
matsudoboku <muramatsu.matsukawa@gmail.com>
2025-07-09 23:47:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -579,8 +579,8 @@ function calculateVCurve(){
   const ghm = gh0 + g1*xm - a*xm*xm;
   const vcr = vcl / (g1 - g2);
   const label = (g1 - g2) > 0 ? '最高地点' : '最低地点';
-  html += `${label}：${minSt.toFixed(2)}m GH：${ghm.toFixed(2)}<br>`  html += `VCR：${vcr.toFixed(2)}<br>`;
-  html += '<table class="survey-table"><tr><th>測点</th><th>GH</th><th>y</th><th>⊿</th></tr>';
+  html += `${label}：${minSt.toFixed(2)}m GH：${ghm.toFixed(2)}<br>`;
+  html += `VCR：${vcr.toFixed(2)}<br>`;  html += '<table class="survey-table"><tr><th>測点</th><th>GH</th><th>y</th><th>⊿</th></tr>';
   let prev = null;
   for(let x=0; x<=vcl; x++){
     const st = start + x;
